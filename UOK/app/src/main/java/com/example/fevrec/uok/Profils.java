733d0d064a42888232e04ad574f3bbff82d60492
Nicolas Beaussart <nic.beaussart@gmail.com>
2016-03-24 13:50:47 +0100

    activityProfils

---
+++ UOK/app/src/main/java/com/example/fevrec/uok/Profils.java
@@ -0,0 +1,14 @@
+package com.example.fevrec.uok;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class Profils extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profils);
+    }
+
+}
