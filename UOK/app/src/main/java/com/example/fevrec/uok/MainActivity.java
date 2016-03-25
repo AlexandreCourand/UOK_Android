3733966c6ea6cc4e019a2b79291f0024fee7ba03
Benoît Verhaeghe <badetitou@gmail.com>
2016-03-25 21:24:11 +0100

    Au revoir menu inutile
    Petit menu inutile d'enlever

--- UOK/app/src/main/java/com/example/fevrec/uok/MainActivity.java
+++ UOK/app/src/main/java/com/example/fevrec/uok/MainActivity.java
@@ -49,29 +49,6 @@ public class MainActivity extends Activity {
         get();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-       /** if (id == R.id.action_settings) {
-            return true;
-        }*/
-
-        return super.onOptionsItemSelected(item);
-    }
-
-
     public void loginOk(View v){
         EditText pseudo = (EditText) findViewById(R.id.pseudo);
         EditText mdp = (EditText) findViewById(R.id.password);
