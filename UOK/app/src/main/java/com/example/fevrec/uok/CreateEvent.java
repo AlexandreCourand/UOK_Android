c309155a6982b21945358bb789c873aac25d4c03
Benoît Verhaeghe <badetitou@gmail.com>
2016-03-27 20:00:24 +0200

    Possible ajout de date avec heure

--- UOK/app/src/main/java/com/example/fevrec/uok/CreateEvent.java
+++ UOK/app/src/main/java/com/example/fevrec/uok/CreateEvent.java
@@ -1,5 +1,7 @@
 package com.example.fevrec.uok;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -17,12 +19,28 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import com.example.fevrec.uok.tools.ContactPickerMulti;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
 public class CreateEvent extends AppCompatActivity {
+
+    private Calendar myCalendar;
+    private EditText name;
+    private EditText description;
+    private EditText price;
+    private EditText address;
+    private EditText dateEvent;
+    private EditText dateLimite;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,14 +53,106 @@ public class CreateEvent extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Mettre requete ici", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        myCalendar = Calendar.getInstance();
+
+        final DatePickerDialog.OnDateSetListener dateEventPopUp = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabelDateEvent();
+            }
+
+        };
+
+        final DatePickerDialog.OnDateSetListener dateEventLimit = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabelDateLimit();
+            }
+
+        };
+
+        name = (EditText) findViewById(R.id.create_event_name);
+        description = (EditText) findViewById(R.id.create_event_description);
+        price = (EditText) findViewById(R.id.create_event_price);
+        address = (EditText) findViewById(R.id.create_event_address);
+        dateEvent = (EditText) findViewById(R.id.create_event_date);
+        dateLimite = (EditText) findViewById(R.id.create_event_limit_date);
+
+
+        dateEvent.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(CreateEvent.this, dateEventPopUp, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+                }
+        });
+
+        dateLimite.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(CreateEvent.this, dateEventLimit, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
             }
         });
     }
 
+    private void updateLabelDateEvent() {
+        String myFormat = "MM/dd/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
 
+        dateEvent.setText(sdf.format(myCalendar.getTime()));
+        int hour = myCalendar.get(Calendar.HOUR_OF_DAY);
+        int minute = myCalendar.get(Calendar.MINUTE);
+        TimePickerDialog mTimePicker;
+        mTimePicker = new TimePickerDialog(CreateEvent.this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                String s = dateEvent.getText().toString() + " " + selectedHour + ":" + selectedMinute;
+                dateEvent.setText(s);
+            }
+        }, hour, minute, true);
+        mTimePicker.setTitle("Select Time");
+        mTimePicker.show();
+    }
 
+    private void updateLabelDateLimit() {
+        String myFormat = "MM/dd/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        dateLimite.setText(sdf.format(myCalendar.getTime()));
+        int hour = myCalendar.get(Calendar.HOUR_OF_DAY);
+        int minute = myCalendar.get(Calendar.MINUTE);
+        TimePickerDialog mTimePicker;
+        mTimePicker = new TimePickerDialog(CreateEvent.this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                String s = dateLimite.getText().toString() + " " + selectedHour + ":" + selectedMinute;
+                dateLimite.setText(s);
+            }
+        }, hour, minute, true);
+        mTimePicker.setTitle("Select Time");
+        mTimePicker.show();
+
+    }
 
 
     public void getInviteList1 (View view){
